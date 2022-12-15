c7f9264b0018fb7f60c98d6c97a93da0feee313b
Alyona <borisovsk_a@mail.ru>
2022-12-15 07:09:50 +0300

    feat: MainAct

--- app/src/main/java/com/example/converterapp/MainActivity.kt
+++ app/src/main/java/com/example/converterapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.converterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.converterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActLog"
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
     }
 }
